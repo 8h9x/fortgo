f56bc14bf71a968a4b1f236c4e60087572b8bee2
Nick Phillips <itsdistrust@icloud.com>
2025-12-12 19:58:00 -0500

    user fetching

--- account.go
+++ account.go
@@ -1,9 +1,12 @@
 package vinderman
 
 import (
+	"fmt"
 	"github.com/8h9x/vinderman/auth"
 	"github.com/8h9x/vinderman/consts"
 	"github.com/8h9x/vinderman/request"
+	"net/http"
+	"time"
 )
 
 func (c *Client) GetExchangeCode() (auth.ExchangeResponse, error) {
@@ -33,4 +36,76 @@ func (c *Client) GetPublicKey(thumbprint string) (GetPublicKeyResponse, error) {
 	}
 
 	return res.Body, err
+}
+
+type FetchUserResponse struct {
+	ID                           string    `json:"id"`
+	DisplayName                  string    `json:"displayName"`
+	Name                         string    `json:"name"`
+	Email                        string    `json:"email"`
+	FailedLoginAttempts          int       `json:"failedLoginAttempts"`
+	LastLogin                    time.Time `json:"lastLogin"`
+	NumberOfDisplayNameChanges   int       `json:"numberOfDisplayNameChanges"`
+	AgeGroup                     string    `json:"ageGroup"`
+	Headless                     bool      `json:"headless"`
+	Country                      string    `json:"country"`
+	LastName                     string    `json:"lastName"`
+	PhoneNumber                  string    `json:"phoneNumber"`
+	Company                      string    `json:"company"`
+	PreferredLanguage            string    `json:"preferredLanguage"`
+	LastDisplayNameChange        time.Time `json:"lastDisplayNameChange"`
+	CanUpdateDisplayName         bool      `json:"canUpdateDisplayName"`
+	TFAEnabled                   bool      `json:"tfaEnabled"`
+	EmailVerified                bool      `json:"emailVerified"`
+	MinorVerified                bool      `json:"minorVerified"`
+	MinorExpected                bool      `json:"minorExpected"`
+	MinorStatus                  string    `json:"minorStatus"`
+	GuardianChallengeTimestamp   time.Time `json:"guardianChallengeTimestamp"`
+	SIWENotificationEnabled      bool      `json:"siweNotificationEnabled"`
+	CabinedMode                  bool      `json:"cabinedMode"`
+	HasHashedEmail               bool      `json:"hasHashedEmail"`
+	LastReviewedSecuritySettings time.Time `json:"lastReviewedSecuritySettings"`
+}
+
+func (c *Client) FetchUserByID(accountID string) (FetchUserResponse, error) {
+	credentials := c.CredentialsMap[c.ClientID]
+
+	headers := http.Header{}
+	headers.Set("Authorization", "Bearer "+credentials.AccessToken)
+
+	resp, err := c.Request("GET", fmt.Sprintf("%s/account/api/public/account/%s", consts.AccountService, accountID), headers, "")
+	if err != nil {
+		return FetchUserResponse{}, err
+	}
+
+	res, err := request.ResponseParser[FetchUserResponse](resp)
+	if err != nil {
+		return FetchUserResponse{}, err
+	}
+
+	return res.Body, err
+}
+
+func (c *Client) FetchUserByDisplayName(displayName string) (FetchUserResponse, error) {
+	credentials := c.CredentialsMap[c.ClientID]
+
+	headers := http.Header{}
+	headers.Set("Authorization", "Bearer "+credentials.AccessToken)
+
+	resp, err := c.Request("GET", fmt.Sprintf("%s/account/api/public/account/displayName/%s", consts.AccountService, displayName), headers, "")
+	if err != nil {
+		return FetchUserResponse{}, err
+	}
+
+	res, err := request.ResponseParser[FetchUserResponse](resp)
+	if err != nil {
+		return FetchUserResponse{}, err
+	}
+
+	return res.Body, err
+}
+
+func (c Client) FetchMe() (FetchUserResponse, error) {
+	credentials := c.CredentialsMap[c.ClientID]
+	return c.FetchUserByID(credentials.AccountID)
 }
--- eos/auth.go
+++ eos/auth.go
@@ -8,29 +8,29 @@ import (
 
 type ClientCredentials struct {
 	AccessToken   string    `json:"access_token"`
-	ApplicationId string    `json:"application_id"`
-	ClientId      string    `json:"client_id"`
+	ApplicationID string    `json:"application_id"`
+	ClientID      string    `json:"client_id"`
 	ExpiresAt     time.Time `json:"expires_at"`
 	ExpiresIn     int       `json:"expires_in"`
 	TokenType     string    `json:"token_type"`
 }
 
 type DeviceAuthorization struct {
-	ClientId                string `json:"client_id"`
+	ClientID                string `json:"client_id"`
 	DeviceCode              string `json:"device_code"`
 	ExpiresIn               int    `json:"expires_in"`
 	Interval                int    `json:"interval"`
 	Prompt                  string `json:"prompt"`
 	UserCode                string `json:"user_code"`
-	VerificationUri         string `json:"verification_uri"`
-	VerificationUriComplete string `json:"verification_uri_complete"`
+	VerificationURI         string `json:"verification_uri"`
+	VerificationURIComplete string `json:"verification_uri_complete"`
 }
 
 type UserCredentials struct {
 	AccessToken      string    `json:"access_token"`
 	AccountID        string    `json:"account_id"`
-	ApplicationId    string    `json:"application_id"`
-	ClientId         string    `json:"client_id"`
+	ApplicationID    string    `json:"application_id"`
+	ClientID         string    `json:"client_id"`
 	ExpiresAt        time.Time `json:"expires_at"`
 	ExpiresIn        int       `json:"expires_in"`
 	RefreshExpiresAt time.Time `json:"refresh_expires_at"`
@@ -42,7 +42,7 @@ type UserCredentials struct {
 
 type Exchange struct {
 	Code             string `json:"code"`
-	CreatingClientId string `json:"creatingClientId"`
+	CreatingClientID string `json:"creatingClientId"`
 	ExpiresInSeconds int    `json:"expiresInSeconds"`
 }
 
