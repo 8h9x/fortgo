b4e9fbea7f96b987635876a6906656f84d36bfee
Nick Phillips <itsdistrust@icloud.com>
2025-12-17 19:19:37 -0500

    finish moving friends svc to new request system

--- friends/blocklist.go
+++ friends/blocklist.go
@@ -28,24 +28,29 @@ func (c *Client) Block(accountID string, targetID string) error {
 	return err
 }
 
-//func (c *Client) GetBlockedList(accountID string) ([]BlockedPlayer, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/blocklist", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []BlockedPlayer{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]BlockedPlayer](resp)
-//	if err != nil {
-//		return []BlockedPlayer{}, err
-//	}
-//
-//	return res.Body, nil
-//}
+func (c *Client) GetBlockedList(accountID string) ([]BlockedPlayer, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/blocklist", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []BlockedPlayer{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []BlockedPlayer{}, err
+	}
+
+	resp, err := request.ParseResponse[[]BlockedPlayer](res)
+	if err != nil {
+		return []BlockedPlayer{}, err
+	}
+
+	return resp.Data, nil
+}
 
 func (c *Client) Unblock(accountID string, targetID string) error {
 	req, err := request.MakeRequest(
--- friends/friends.go
+++ friends/friends.go
@@ -3,32 +3,39 @@ package friends
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/8h9x/fortgo/consts"
 	"github.com/8h9x/fortgo/request"
 )
 
-//func (c *Client) AcceptBulk(accountID string, targetIDs []string) ([]string, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	query := url.Values{}
-//	query.Set("targetIds", strings.Join(targetIDs, ","))
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/incoming/accept%s", consts.FriendsService, accountID, query.Encode())
-//
-//	resp, err := request.Request(c.HTTPClient, "POST", reqUrl, headers, "")
-//	if err != nil {
-//		return []string{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]string](resp)
-//	if err != nil {
-//		return []string{}, err
-//	}
-//
-//	return res.Body, nil
-//}
+func (c *Client) AcceptBulk(accountID string, targetIDs []string) ([]string, error) {
+	query := url.Values{}
+	query.Set("targetIds", strings.Join(targetIDs, ","))
+
+	req, err := request.MakeRequest(
+		http.MethodPost,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/incoming/accept?%s", accountID, query.Encode()),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []string{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []string{}, err
+	}
+
+	resp, err := request.ParseResponse[[]string](res)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return resp.Data, nil
+}
 
 func (c *Client) AddFriend(accountID string, targetID string, parentalControlsPin string) error {
 	req, err := request.MakeRequest(
@@ -51,157 +58,197 @@ func (c *Client) AddFriend(accountID string, targetID string, parentalControlsPi
 	return err
 }
 
-//func (c *Client) GetFriend(accountID string, friendID string) (Friend, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/friends/%s", consts.FriendsService, accountID, friendID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return Friend{}, err
-//	}
-//
-//	res, err := request.ResponseParser[Friend](resp)
-//	if err != nil {
-//		return Friend{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetFriendsList(accountID string) ([]Friend, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/friends", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []Friend{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]Friend](resp)
-//	if err != nil {
-//		return []Friend{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetMutual(accountID string, friendID string) ([]string, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/friends/%s/mutual", consts.FriendsService, accountID, friendID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []string{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]string](resp)
-//	if err != nil {
-//		return []string{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetSummary(accountID string) (FriendSummaryResponse, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/summary", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return FriendSummaryResponse{}, err
-//	}
-//
-//	res, err := request.ResponseParser[FriendSummaryResponse](resp)
-//	if err != nil {
-//		return FriendSummaryResponse{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetSuggested(accountID string) ([]SuggestedFriend, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/suggested", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []SuggestedFriend{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]SuggestedFriend](resp)
-//	if err != nil {
-//		return []SuggestedFriend{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetRelations(accountID string) (any, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/relations", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return struct{}{}, err
-//	}
-//
-//	res, err := request.ResponseParser[any](resp)
-//	if err != nil {
-//		return struct{}{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetRequestsIncoming(accountID string) ([]FriendRequest, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/incoming", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []FriendRequest{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]FriendRequest](resp)
-//	if err != nil {
-//		return []FriendRequest{}, err
-//	}
-//
-//	return res.Body, nil
-//}
-//
-//func (c *Client) GetRequestsOutgoing(accountID string) ([]FriendRequest, error) {
-//	headers := http.Header{}
-//	headers.Set("Authorization", "Bearer "+c.Credentials.AccessToken)
-//
-//	reqUrl := fmt.Sprintf("%s/friends/api/v1/%s/outgoing", consts.FriendsService, accountID)
-//
-//	resp, err := request.Request(c.HTTPClient, "GET", reqUrl, headers, "")
-//	if err != nil {
-//		return []FriendRequest{}, err
-//	}
-//
-//	res, err := request.ResponseParser[[]FriendRequest](resp)
-//	if err != nil {
-//		return []FriendRequest{}, err
-//	}
-//
-//	return res.Body, nil
-//}
+func (c *Client) GetFriend(accountID string, friendID string) (Friend, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/friends/%s", accountID, friendID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return Friend{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return Friend{}, err
+	}
+
+	resp, err := request.ParseResponse[Friend](res)
+	if err != nil {
+		return Friend{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetFriendsList(accountID string) ([]Friend, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/friends", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []Friend{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []Friend{}, err
+	}
+
+	resp, err := request.ParseResponse[[]Friend](res)
+	if err != nil {
+		return []Friend{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetMutual(accountID string, friendID string) ([]string, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/friends/%s/mutual", accountID, friendID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []string{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []string{}, err
+	}
+
+	resp, err := request.ParseResponse[[]string](res)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetSummary(accountID string) (FriendSummaryResponse, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/summary", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return FriendSummaryResponse{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return FriendSummaryResponse{}, err
+	}
+
+	resp, err := request.ParseResponse[FriendSummaryResponse](res)
+	if err != nil {
+		return FriendSummaryResponse{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetSuggested(accountID string) ([]SuggestedFriend, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/suggested", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	resp, err := request.ParseResponse[[]SuggestedFriend](res)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetRelations(accountID string) (any, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/relations", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	resp, err := request.ParseResponse[[]SuggestedFriend](res)
+	if err != nil {
+		return []SuggestedFriend{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetRequestsIncoming(accountID string) ([]FriendRequest, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/incoming", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	resp, err := request.ParseResponse[[]FriendRequest](res)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	return resp.Data, nil
+}
+
+func (c *Client) GetRequestsOutgoing(accountID string) ([]FriendRequest, error) {
+	req, err := request.MakeRequest(
+		http.MethodGet,
+		consts.FriendsService,
+		fmt.Sprintf("friends/api/v1/%s/outgoing", accountID),
+		request.WithBearerToken(c.Credentials.AccessToken),
+	)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	resp, err := request.ParseResponse[[]FriendRequest](res)
+	if err != nil {
+		return []FriendRequest{}, err
+	}
+
+	return resp.Data, nil
+}
 
 func (c *Client) RemoveAllFriends(accountID string) error {
 	req, err := request.MakeRequest(
